9671156ad98d55be5288cb787a8af9dcc935dc98
masyrabie-code <masyrabie@gmail.com>
2026-07-04 05:11:15 +0300

    Add Footer component with styling and copyright text

---
+++ frontend/components/layout/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "40px",
+        textAlign: "center",
+        borderTop: "1px solid #E2E8F0",
+        marginTop: "100px",
+      }}
+    >
+      © 2026 SkillNova. All rights reserved.
+    </footer>
+  );
+}
